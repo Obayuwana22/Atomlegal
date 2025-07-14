46f0c99f3595a518095bf6ae2c69be6a5ee89c5f
success obayuwana <successobayuwana@gmail.com>
2025-07-14 06:50:54 +0100

    Change font

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import HomeLayout from "./components/HomeLayout";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen bg-white overflow-hidden">
+    <div className="flex flex-col min-h-screen bg-white overflow-hidden font-display">
       <HomeLayout />
     </div>
   );
